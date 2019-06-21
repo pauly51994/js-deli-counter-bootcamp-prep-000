7eace6505a9905d5ebace027351d22ece8ce337d
Paul Yun <p.yun1994@gmail.com>
2019-06-21 23:47:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,8 +13,10 @@ function nowServing(line){
 function currentLine(line){
   if (line.length === 0){return 'The line is currently empty.'}
   else {
+    let arr = [];
     for (let i = 0; i < line.length; i++){
-
+      arr.push(`${i+1}. line[i]`);
     }
+    return arr.join(', ');
   }
 }
